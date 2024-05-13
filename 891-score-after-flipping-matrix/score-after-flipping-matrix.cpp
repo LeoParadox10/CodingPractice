3cdb1c54f4673fdc8276782ded6f5f03ad97328f
Smarak Patra <43983935+LeoParadox10@users.noreply.github.com>
2024-05-13 12:31:10 +0530

    Time: 2 ms (54.61%) | Memory: 10.2 MB (75.07%) - LeetSync

---
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        for(int i=0;i<m;i++)
+        {
+            if(grid[i][0]==0)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(grid[i][j]==0)
+                        grid[i][j]=1;
+                    else
+                        grid[i][j]=0;
+                }
+            }
+            else
+                continue;
+        }
+        for(int j=0;j<n;j++)
+        {
+            int one=0;
+            int zero=0;
+            for(int i=0;i<m;i++)
+            {
+                if(grid[i][j]==0)
+                    zero++;
+                else
+                    one++;
+            }
+            if(zero>one)
+            {
+                for(int i=0;i<m;i++)
+                {
+                    if(grid[i][j]==0)
+                        grid[i][j]=1;
+                    else
+                        grid[i][j]=0;
+                }
+            }
+        }
+        int sum=0;
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                sum+=grid[i][j]*(1<<(n-1-j));
+        return sum;
+    }
+};
