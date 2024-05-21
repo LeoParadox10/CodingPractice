db352228b705d6b356cc53c41bf708a208311939
Smarak Patra <43983935+LeoParadox10@users.noreply.github.com>
2024-05-21 11:35:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (21.93%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    void f(vector<vector<int>> &ans, int ind, vector<int> curr, vector<int> &nums)
+    {
+        if(ind==-1)
+        {
+            ans.push_back(curr);
+            return;
+        }
+        f(ans, ind-1, curr, nums);
+        curr.push_back(nums[ind]);
+        f(ans, ind-1, curr, nums);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+        vector<int> curr;
+        f(ans, n-1, curr, nums);
+        return ans;
+    }
+};
