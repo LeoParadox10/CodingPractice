1c4c7e8e2dea5a25b9a5e1b8b7dc1e6805fed6fb
Smarak Patra <43983935+LeoParadox10@users.noreply.github.com>
2024-07-11 18:03:02 +0530

    Time: 5 ms (59.35%) | Memory: 16.4 MB (15.35%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n=nums.size();
-        int max_pro = nums[0];
-        long long max_so_far = nums[0], min_so_far = nums[0];
+        double preff = 1;
+        double suff = 1;
+        double ans = INT_MIN;
+        int n = nums.size();
         
-        for (int i = 1; i < n; i++) {
-            if (nums[i] < 0) {
-                swap(max_so_far, min_so_far);
-            }
-            
-            max_so_far = max((long long)nums[i], max_so_far * nums[i]);
-            min_so_far = min((long long)nums[i], min_so_far * nums[i]);
-            if (max_so_far > LLONG_MAX / 10 || min_so_far < LLONG_MIN / 10) {
-                max_so_far = nums[i];
-                min_so_far = nums[i];
-            }
-            
-            max_pro = max(max_pro, (int)max_so_far);
+        for (int i = 0; i < n; i++) {
+            if (preff == 0) preff = 1;
+            if (suff == 0) suff = 1;
+            preff *= nums[i];
+            suff *= nums[n - i - 1];
+            ans = max(ans, max(preff, suff));
         }
-        return max_pro;
+        
+        return (int)ans;
     }
 };
