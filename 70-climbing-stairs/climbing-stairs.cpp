57346f5dc96628c110bd4323eecbb9d5655c8efb
Smarak Patra <43983935+LeoParadox10@users.noreply.github.com>
2024-08-06 11:18:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (18.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    int f(int ind, vector<int> &dp) {
-        if (ind == 0)
+    int f(int n, vector<int> &dp)
+    {
+        if(n==0 || n==1)
             return 1;
-        if (ind == 1)
-            return 1;
-        if (dp[ind] != -1)
-            return dp[ind];
-        int left = f(ind - 1, dp);
-        int right = f(ind - 2, dp);
-        return dp[ind] = left + right;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n]=f(n-1, dp)+f(n-2, dp);
     }
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
