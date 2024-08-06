2b8dc4acccc7d23c5749feb49c5a0581584aaf76
Smarak Patra <43983935+LeoParadox10@users.noreply.github.com>
2024-08-06 12:56:54 +0530

    Time: 45 ms (37.95%) | Memory: 51 MB (14.39%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int f(int ind, vector<int> &coins, int k, vector<vector<int>> &dp)
-    {
-        if(ind==0)
-        {
-            if(k%coins[ind]==0)
-                return dp[ind][k]=k/coins[ind];
-            return 1e9;
-        }
-        if(dp[ind][k]!=-1)
-            return dp[ind][k];
-        int notPick=0+f(ind-1, coins, k, dp);
-        int pick=INT_MAX;
-        if(coins[ind]<=k)
-            pick=1+f(ind, coins, k-coins[ind], dp);
-        return dp[ind][k]=min(pick, notPick);
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
-        int ways=f(n-1, coins, amount, dp);
-        if(ways>=1e9)
+        vector<vector<int>> dp(n+1, vector<int> (amount+1, 0));
+        for(int k=0;k<=amount;k++)
+        {   
+            if(k%coins[0]==0)
+                dp[0][k]=k/coins[0];
+            else
+                dp[0][k]=1e9;
+        }
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int k=0;k<=amount;k++)
+            {
+                int notPick=0+dp[ind-1][k];
+                int pick=INT_MAX;
+                if(coins[ind]<=k)
+                    pick=1+dp[ind][k-coins[ind]];
+                dp[ind][k]=min(pick, notPick);
+            }
+        }
+        int ways=dp[n-1][amount];
+        if(ways==1e9)
             return -1;
         return ways;
     }
