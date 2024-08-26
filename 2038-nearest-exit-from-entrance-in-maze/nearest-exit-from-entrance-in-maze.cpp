249fda13c1746de9bd684ab8f21e4c2e19be6dca
Smarak Patra <43983935+LeoParadox10@users.noreply.github.com>
2024-08-26 21:52:21 +0530

    Time: 92 ms (61.55%) | Memory: 35.5 MB (53.20%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -1,45 +1,46 @@
 class Solution {
 private:
-    bool isValid(vector<vector<char>> &maze, vector<vector<bool>> &vis, int x, int y) {
-        int n = maze.size(), m = maze[0].size();
-        if (x >= 0 && x < n && y >= 0 && y < m && maze[x][y] == '.' && !vis[x][y])
+    bool isValid(vector<vector<char>> &maze, vector<vector<bool>> &vis, int x, int y)
+    {
+        int n=maze.size(), m=maze[0].size();
+        if(x>=0 && x<n && y>=0 && y<m && maze[x][y]=='.' && vis[x][y]==0)
             return true;
         return false;
     }
-
-    bool reachEnd(vector<vector<char>> &maze, int x, int y) {
-        int n = maze.size(), m = maze[0].size();
-        if (x == 0 || x == n - 1 || y == 0 || y == m - 1)
+    bool reachEnd(vector<vector<char>> &maze, int x, int y)
+    {
+        int n=maze.size(), m=maze[0].size();
+        if(x==0 || x==n-1 || y==0 || y==m-1)
             return true;
         return false;
     }
-
 public:
-    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int n = maze.size();
-        int m = maze[0].size();
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance)
+    {
+        int n=maze.size();
+        int m=maze[0].size();
         vector<vector<bool>> vis(n, vector<bool>(m, false));
         queue<pair<pair<int, int>, int>> q;
         q.push({{entrance[0], entrance[1]}, 1});
-        vis[entrance[0]][entrance[1]] = true;
-        int dx[] = {-1, 1, 0, 0};
-        int dy[] = {0, 0, -1, 1};
-
-        while (!q.empty()) {
-            int cnt = q.front().second;
-            int x = q.front().first.first;
-            int y = q.front().first.second;
+        vis[entrance[0]][entrance[1]]=true;
+        int dx[]={-1, 1, 0, 0};
+        int dy[]={0, 0, -1, 1};
+        while(!q.empty())
+        {
+            int cnt=q.front().second;
+            int x=q.front().first.first;
+            int y=q.front().first.second;
             q.pop();
-
-            for (int i = 0; i < 4; i++) {
-                int newX = x + dx[i];
-                int newY = y + dy[i];
-
-                if (isValid(maze, vis, newX, newY)) {
-                    if (reachEnd(maze, newX, newY))
+            for(int i=0;i<4;i++)
+            {
+                int newx=x+dx[i];
+                int newy=y+dy[i];
+                if(isValid(maze, vis, newx, newy))
+                {
+                    if(reachEnd(maze, newx, newy))
                         return cnt;
-                    vis[newX][newY] = true;
-                    q.push({{newX, newY}, cnt + 1});
+                    vis[newx][newy]=true;
+                    q.push({{newx, newy}, cnt+1});
                 }
             }
         }
