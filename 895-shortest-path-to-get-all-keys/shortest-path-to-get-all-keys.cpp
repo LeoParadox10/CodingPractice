cc90309c122b3fe2b4a29a085040d7a05704bb7d
Smarak Patra <43983935+LeoParadox10@users.noreply.github.com>
2024-09-01 03:12:40 +0530

    Time: 490 ms (7.94%) | Memory: 96.6 MB (6.98%) - LeetSync

--- 895-shortest-path-to-get-all-keys/shortest-path-to-get-all-keys.cpp
+++ 895-shortest-path-to-get-all-keys/shortest-path-to-get-all-keys.cpp
@@ -17,6 +17,7 @@ public:
         int m=grid[0].size();
         int keys=0;
         int x, y, c=0;
+        // Locate the start position and count the total number of keys
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
@@ -40,12 +41,18 @@ public:
                 int col=q.front().first.second;
                 string keys=q.front().second;
                 q.pop();
+
+                // If we collected all keys
                 if(keys.size()==c)
                     return moves;
+
                 string key=to_string(row)+":"+to_string(col)+":"+keys;
                 if(vis.find(key)!=vis.end())
                     continue;
+
                 vis.insert(key);
+
+                // Explore 4 directions
                 for(int j=0;j<4;j++)
                 {
                     int nrow=row+dx[j];
@@ -53,10 +60,16 @@ public:
                     if(nrow<0 || nrow>=n || ncol<0 || ncol>=m || grid[nrow][ncol]=='#')
                         continue;
                     char ch=grid[nrow][ncol];
+
+                    // If we encounter a lock and have the corresponding key
                     if(ch>='A' && ch<='F' && present(ch+32, keys))
                         q.push({{nrow, ncol}, keys});
+
+                    // If we encounter an empty cell or the start position
                     else if(ch=='.' || ch=='@')
                         q.push({{nrow, ncol}, keys});
+
+                    // If we encounter a key
                     else if(ch>='a' && ch<='f')
                     {
                         if(present(ch, keys))
