3355ca49b574a969f50de6b17531da9ce466f331
Smarak Patra <43983935+LeoParadox10@users.noreply.github.com>
2024-08-31 19:35:29 +0530

    Time: 15 ms (69.69%) | Memory: 19.1 MB (14.43%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,39 +1,34 @@
 class Solution {
-private:
-    bool dfsCheck(int node, vector<vector<int>> adj, vector<int> &vis, vector<int> &pathVis)
+    private:
+    bool dfs(int node, vector<vector<int>> &adjL, vector<bool> &vis, vector<bool> &pathVis)
     {
-        vis[node]=1;
-        pathVis[node]=1;
-        // traverse for adjacent nodes
-        for(auto neighbour:adj[node])
+        vis[node]=true;
+        pathVis[node]=true;
+        for(auto neighbour:adjL[node])
         {
-            // when node is visited
             if(!vis[neighbour])
             {
-                if(dfsCheck(neighbour, adj, vis, pathVis)==true)
+                if(dfs(neighbour, adjL, vis, pathVis))
                     return true;
             }
-            // if the node has been previously visited
-            // but it has to bee visited on the same path
             else if(pathVis[neighbour])
                 return true;
         }
-        pathVis[node]=0;
+        pathVis[node]=false;
         return false;
     }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> vis(numCourses, 0);
-        vector<int> pathVis(numCourses, 0);
-        vector<vector<int>> adj(numCourses);
-        for(auto &prerequisite : prerequisites) {
-            adj[prerequisite[1]].push_back(prerequisite[0]);
-        }
+        vector<bool> vis(numCourses, false);
+        vector<bool> pathVis(numCourses, false);
+        vector<vector<int>> adjL(numCourses);
+        for(auto requisite:prerequisites)
+            adjL[requisite[1]].push_back(requisite[0]);
         for(int i=0;i<numCourses;i++)
         {
             if(!vis[i])
             {
-                if(dfsCheck(i, adj, vis, pathVis)==true)
+                if(dfs(i, adjL, vis, pathVis))
                     return false;
             }
         }
