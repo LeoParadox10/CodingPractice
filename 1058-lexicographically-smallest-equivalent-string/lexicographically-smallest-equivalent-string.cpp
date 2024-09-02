e092db8e3ed1d13b311f395070ee9785645401fa
Smarak Patra <43983935+LeoParadox10@users.noreply.github.com>
2024-09-03 02:04:48 +0530

    Time: 4 ms (62.11%) | Memory: 7.9 MB (95.68%) - LeetSync

---
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int parent[26];
+    int find(int u)
+    {
+        return parent[u]==u?u:find(parent[u]);
+        if(parent[u]=u)
+            return u;
+        return parent[u]=find(parent[u]);
+    }
+    
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        for(int i=0;i<26;i++)
+            parent[i]=i;
+        for(int i=0;i<s1.size();i++)
+        {
+            int u=find(s1[i]-'a');
+            int v=find(s2[i]-'a');
+            if(u<v)
+                parent[v]=u;
+            else if(u>v)
+                parent[u]=v;
+            else
+                continue;
+        }
+        for(int i=0;i<baseStr.size();i++)
+            baseStr[i]=(char)(find(baseStr[i]-'a')+'a');
+        return baseStr;
+    }
+};
